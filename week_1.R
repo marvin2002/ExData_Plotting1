83baaeb34f2c65b63988a4c12bd19e78022330c2
marvin2002 <marotzoy@gmail.com>
2017-01-22 18:25:29 -0600

    Update week_1.R

--- week_1.R
+++ week_1.R
@@ -1,6 +1,6 @@
 
  
- ##
+ 
 hpc <-  read.csv("household_power_consumption.txt", sep = ";", stringsAsFactors = FALSE)
 
 
@@ -20,7 +20,7 @@ hist(as.numeric(hpc_data$Global_active_power), col = "red", xlab = "Global Activ
 dev.off()
 
 #PLOT NO. 2
-#Sys.setlocale("LC_TIME", "English")
+
 png("plot2.png")
 plot( hpc_dat$DateTime,  hpc_dat$Global_active_power, ylab = "Global Active Power (kilowatts)", xlab= "", type="l")
 dev.off()
@@ -39,8 +39,8 @@ legend("topright", legend = c("sub_metering_1", "sub_metering_2", "sub_metering_
 dev.off()
 
 #PLOT NO. 4
-par(mfrow= c(2,2))
 png("plot4.png")
+par(mfrow= c(2,2))
 plot( hpc_dat$DateTime,  hpc_dat$Global_active_power, ylab = "Global Active Power (kilowatts)", xlab= "", type="l")
 plot(hpc_dat$DateTime, hpc_dat$Voltage, xlab = "datetime", ylab = "Voltage", type = "l" )
 
@@ -48,9 +48,8 @@ plot(hpc_dat$DateTime, hpc_dat$Voltage, xlab = "datetime", ylab = "Voltage", typ
   plot(hpc_dat$DateTime, hpc_dat$Sub_metering_1, type= "l", xlab = "", ylab = "Energy sub metering")
   lines(hpc_dat$DateTime, hpc_dat$Sub_metering_2, type= "l", col= "red")
   lines(hpc_dat$DateTime, hpc_dat$Sub_metering_3, type= "l", col="blue")
-  legend(x= c(hpc_dat$DateTime[1900], hpc_dat$DateTime[2880]), y= c(20,40), legend = c("sub_metering_1", "sub_metering_2", "sub_metering_3"),
-         lty = c(1, 1), col= c("black", "red", "blue"), cex = 0.5, lwd = c(2,2,2))
-
+  legend(x= c(hpc_dat$DateTime[1500], hpc_dat$DateTime[2880]), y= c(30,45), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+    lty = c(1, 1), col= c("black", "red", "blue"), cex = 0.6, lwd = c(1,1,1), y.intersp = 0.5, bty = "n")
 
 
 plot(hpc_dat$DateTime, hpc_dat$Global_reactive_power, xlab = "datetime", ylab = "Global_reactive_power",
